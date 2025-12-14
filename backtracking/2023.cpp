f4102573c095f5011f0c6fea95271d8effea8513
Jieun Park <140596167+jieunpark626@users.noreply.github.com>
2025-12-15 02:45:48 +0900

    boj2023

---
+++ backtracking/2023.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+
+int change(vector<int> num){
+    int result=0;
+    for(int i=0; i<num.size(); i++){
+        result = result*10 + num[i];
+    }
+    return result;
+}
+
+bool is_prime(vector<int> arr){
+    int num = change(arr);
+    if(num==0||num==1) return false;
+    if(num==2) return true;
+    if(num%2==0) return false;
+    for(int i=3; i<num; i+=2){
+        if(num%i==0) return false;
+    }
+    return true;
+}
+
+void solve(vector<int> num){
+    if(num.size()!=0 && !is_prime(num)) 
+        return;
+
+    if(num.size()==n){
+        for(int i=0; i<n; i++){
+            cout<<num[i];
+        }
+        cout<<"\n";
+        return;
+    }
+
+    for(int i=0; i<=9; i++){
+        num.push_back(i);
+        solve(num);
+        num.pop_back();
+    }
+}
+
+int main(){
+    cin>>n;
+    vector<int> num;
+    solve(num);
+    return 0;
+}
