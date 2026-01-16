2a9f3fea2bde728829cd64be9dfe804c23e8fec9
Jieun Park <140596167+jieunpark626@users.noreply.github.com>
2026-01-16 15:51:06 +0900

    re: boj12852

--- dp/12852.cpp
+++ dp/12852.cpp
@@ -1,48 +1,52 @@
 #include <iostream>
+#include <queue>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-struct node{
-    int cnt=0;
-    vector<int> list;
-};
-const int MAX = 987654321;
-int n;
-node dp[1000000*3+1];
+int N;
 
 int main(){
-    cin>>n;
-
-    for(int i=0; i<=n; i++){
-        dp[i] = {MAX};
-    }
-    dp[1]={0};
-
-    for(int i=1; i<=n; i++){
-        int count = dp[i].cnt + 1;
-
-        if(dp[i+1].cnt > count) {
-            dp[i+1].cnt = count;
-            dp[i+1].list = dp[i].list; dp[i+1].list.push_back(i);
-        }
-
-        if(dp[i*2].cnt > count){
-            dp[i*2].cnt = count;
-            dp[i*2].list = dp[i].list; dp[i*2].list.push_back(i);
-        }
-
-        if(dp[i*3].cnt > count){
-            dp[i*3].cnt = count;
-            dp[i*3].list = dp[i].list; dp[i*3].list.push_back(i);
-        }
-    }
-
-    cout<<dp[n].cnt<<"\n"<<n<<" ";
-
-    for(int i=dp[n].list.size()-1; i>=0; i--){
-        cout<<dp[n].list[i]<<" ";
-    }
+    cin>>N;
+    vector<int> p(N+1,-1);
     
+    queue<int> q;
+    q.push(N);
+    
+    while(!q.empty()){
+        int cur = q.front(); q.pop();
+
+        if(cur==1) break;
+
+        if(cur%3==0 && p[cur/3]==-1){
+            p[cur/3] = cur;
+            q.push(cur/3);
+        }
+        if(cur%2==0 && p[cur/2]==-1){
+            p[cur/2] = cur;
+            q.push(cur/2);
+        }
+        if(p[cur-1]==-1){
+            p[cur-1] = cur;
+            q.push(cur-1);
+        }
+    }
+
+    int cur = 1;
+    vector<int> v;
+    
+    v.push_back(1);
+    while(cur!=N){
+        v.push_back(p[cur]);
+        cur=p[cur];
+    }
+
+    reverse(v.begin(), v.end());
+    cout<<v.size()-1<<endl;
+    for(int n: v){
+        cout<<n<<" ";
+    }
+
     return 0;
 }
